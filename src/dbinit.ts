1c376827e3b5116856b0159217565f014e76aa80
WKHAllen <wkhallen@gmail.com>
2020-11-25 15:28:42 -0600

    Fix SQL indentation

--- src/dbinit.ts
+++ src/dbinit.ts
@@ -10,11 +10,11 @@ async function wait(ms: number): Promise<void> {
 export default async function initDB() {
   // Create tables
   const mainTable = `
-		CREATE TABLE IF NOT EXISTS Main (
-			id SERIAL PRIMARY KEY,
-			message TEXT NOT NULL
-		);
-	`;
+    CREATE TABLE IF NOT EXISTS Main (
+      id SERIAL PRIMARY KEY,
+      message TEXT NOT NULL
+    );
+  `;
 
   await mainDB.executeMany([mainTable]);
   await wait(1000);
