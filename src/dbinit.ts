1329ebdba389713544086959cb32dfd9b0e8e205
WKHAllen <wkhallen@gmail.com>
2020-11-28 16:41:32 -0600

    closes #10

--- src/dbinit.ts
+++ src/dbinit.ts
@@ -11,13 +11,21 @@ async function wait(ms: number): Promise<void> {
 export async function populateTable(
   table: string,
   column: string,
-  values: any[]
+  values: any[],
+  other: boolean = false,
+  otherID: number = 1000
 ): Promise<void> {
   const rows = await mainDB.execute(`SELECT ${column} from ${table}`);
+
   if (rows.length === 0) {
-    const queryValues = values.map(
+    let queryValues = values.map(
       (value, index) => `(${index + 1}, '${value}')`
     );
+
+    if (other) {
+      queryValues.push(`(${otherID}, 'Other')`);
+    }
+
     const queryValuesStr = queryValues.join(", ");
     const query = `INSERT INTO ${table} (id, ${column}) VALUES ${queryValuesStr};`;
     await mainDB.execute(query);
@@ -38,6 +46,14 @@ export default async function initDB(): Promise<void> {
   `;
   const userStatusTable = `
     CREATE TABLE IF NOT EXISTS UserStatus (
+      id   INT         NOT NULL,
+      name VARCHAR(63) NOT NULL,
+
+      PRIMARY KEY (id)
+    );
+  `;
+  const LocationTypeTable = `
+    CREATE TABLE IF NOT EXISTS LocationType (
       id   INT         NOT NULL,
       name VARCHAR(63) NOT NULL,
 
@@ -70,16 +86,17 @@ export default async function initDB(): Promise<void> {
   `;
   const postTable = `
     CREATE TABLE IF NOT EXISTS Post (
-      id           CHAR(4)       NOT NULL,
-      userID       CHAR(4)       NOT NULL,
-      content      VARCHAR(750)  NOT NULL,
-      imageID      CHAR(4)       NOT NULL,
-      location     VARCHAR(255)  NOT NULL,
-      program      VARCHAR(255)  NOT NULL,
-      rating       TINYINT       NOT NULL,
-      threeWords   VARCHAR(63)   NOT NULL,
-      createTime   INT UNSIGNED  NOT NULL,
-      editTime     INT UNSIGNED,
+      id             CHAR(4)       NOT NULL,
+      userID         CHAR(4)       NOT NULL,
+      content        VARCHAR(750)  NOT NULL,
+      imageID        CHAR(4)       NOT NULL,
+      location       VARCHAR(255)  NOT NULL,
+      locationTypeID INT           NOT NULL,
+      program        VARCHAR(255)  NOT NULL,
+      rating         TINYINT       NOT NULL,
+      threeWords     VARCHAR(63)   NOT NULL,
+      createTime     INT UNSIGNED  NOT NULL,
+      editTime       INT UNSIGNED,
 
       PRIMARY KEY (id),
 
@@ -87,12 +104,16 @@ export default async function initDB(): Promise<void> {
         REFERENCES User (id),
 
       FOREIGN KEY (imageID)
-        REFERENCES Image (id)
+        REFERENCES Image (id),
+      
+      FOREIGN KEY (locationTypeID)
+        REFERENCES LocationType (id)
     );
   `;
   await mainDB.executeMany([
     imageTable,
     userStatusTable,
+    LocationTypeTable,
     userTable,
     postTable,
   ]);
@@ -118,13 +139,30 @@ export default async function initDB(): Promise<void> {
   // await mainDB.executeMany([userDeleteTrigger, postDeleteTrigger]);
 
   // Populate static tables
-  await populateTable("UserStatus", "name", [
-    "Student",
-    "Alum",
-    "Staff",
-    "Parent",
-    "Other",
-  ]);
+  await populateTable(
+    "UserStatus",
+    "name",
+    ["Student", "Alum", "Staff", "Parent", "Other"],
+    true
+  );
+  await populateTable(
+    "LocationType",
+    "name",
+    [
+      "Hotel",
+      "Hostel",
+      "B&B/Inn",
+      "Cafe/Bakery",
+      "Bar/Pub",
+      "Restaurant",
+      "Museum",
+      "Arts venue",
+      "Sports venue",
+      "Cultural attraction",
+      "Historical attraction",
+    ],
+    true
+  );
 
   const rows = await mainDB.execute("SELECT id FROM Main;");
   const message = "Hello, world!";
