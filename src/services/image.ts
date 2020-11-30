279feb3f4d90dbcd55bab8d4c4aa35f633732e66
WKHAllen <wkhallen@gmail.com>
2020-11-30 14:34:08 -0600

    Implement create image service

--- src/services/image.ts
+++ src/services/image.ts
@@ -1,4 +1,4 @@
-import mainDB from "./util";
+import mainDB, { getTime, newUniqueID } from "./util";
 
 // Image architecture
 export interface Image {
@@ -9,6 +9,17 @@ export interface Image {
 
 // Image services
 export module ImageService {
+  // Create an image
+  export async function createImage(data: Buffer): Promise<string> {
+    const imageID = await newUniqueID("Image");
+
+    const sql = `INSERT INTO Image (id, data, registerTime) VALUES (?, ?, ?);`;
+    const params = [imageID, data, getTime()];
+    await mainDB.execute(sql, params);
+
+    return imageID;
+  }
+
   // Delete an image
   export async function deleteImage(id: string): Promise<void> {
     const sql = `DELETE FROM Image WHERE id = ?;`;
--- src/services/util.ts
+++ src/services/util.ts
@@ -1,8 +1,46 @@
 import * as db from "../db";
+import * as crypto from "crypto";
 
 // Constants
 export const dbURL = process.env.DATABASE_URL;
+const idLength = 4;
 
 // Database object
 const mainDB = new db.DB(dbURL);
 export default mainDB;
+
+// Get the current timestamp
+export function getTime(): number {
+  return Math.floor(new Date().getTime() / 1000);
+}
+
+// Generate a new ID
+export async function newID(): Promise<string> {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(idLength, (err, buffer) => {
+      if (err) {
+        reject(err);
+      } else {
+        let base64ID = buffer.toString("base64").slice(0, length);
+        while (base64ID.includes("/")) base64ID = base64ID.replace("/", "-");
+        while (base64ID.includes("+")) base64ID = base64ID.replace("+", "_");
+        resolve(base64ID);
+      }
+    });
+  });
+}
+
+// Generate a new unique ID for a table
+export async function newUniqueID(table: string): Promise<string> {
+  let base64ID = await newID();
+
+  const sql = `SELECT id FROM ${table} WHERE id = ?;`;
+  let rows = await mainDB.execute(sql, [base64ID]);
+
+  while (rows.length > 0) {
+    base64ID = await newID();
+    rows = await mainDB.execute(sql, [base64ID]);
+  }
+
+  return base64ID;
+}
