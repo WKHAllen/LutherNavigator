b2d02e05b36aa530895bc085ad27b6942f2be9e5
WKHAllen <wkhallen@gmail.com>
2020-11-30 20:11:12 -0600

    Fix test object matching

--- test/services.test.ts
+++ test/services.test.ts
@@ -15,5 +15,11 @@ afterAll(() => {
 
 test("User status", async () => {
   const statuses = await UserStatusService.getStatuses();
-  console.log(statuses);
+  expect(statuses).toMatchObject([
+    { id: 1, name: "Student" },
+    { id: 2, name: "Alum" },
+    { id: 3, name: "Faculty/Staff" },
+    { id: 4, name: "Parent" },
+    { id: 1000, name: "Other" },
+  ]);
 });
