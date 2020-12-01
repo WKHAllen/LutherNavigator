2e23d932cb70987bd60a64e185c460e7ce661350
WKHAllen <wkhallen@gmail.com>
2020-11-30 20:42:11 -0600

    Test location type service

--- test/services.test.ts
+++ test/services.test.ts
@@ -1,6 +1,7 @@
 import mainDB from "../src/services/util";
 import initDB from "../src/dbinit";
 import { UserStatusService } from "../src/services/userStatus";
+import { LocationTypeService } from "../src/services/locationType";
 
 // Setup
 beforeAll(
@@ -24,7 +25,7 @@ test("User status", async () => {
     { id: 2, name: "Alum" },
     { id: 3, name: "Faculty/Staff" },
     { id: 4, name: "Parent" },
-    { id: 1000, name: "Other" },
+    { id: 1000, name: "Other" }
   ]);
 
   // Get status names
@@ -44,4 +45,42 @@ test("User status", async () => {
   expect(validStatus).toBe(true);
   validStatus = await UserStatusService.validStatus(999);
   expect(validStatus).toBe(false);
+});
+
+// Test location type service
+test("Location type", async () => {
+  // Get locations
+  const locations = await LocationTypeService.getLocations();
+  expect(locations).toMatchObject([
+    { id: 1, name: "Hotel" },
+    { id: 2, name: "Hostel" },
+    { id: 3, name: "B&B/Inn" },
+		{ id: 4, name: "Cafe/Bakery" },
+		{ id: 5, name: "Bar/Pub" },
+		{ id: 6, name: "Restaurant" },
+		{ id: 7, name: "Museum" },
+		{ id: 8, name: "Arts venue" },
+		{ id: 9, name: "Sports venue" },
+		{ id: 10, name: "Cultural attraction" },
+		{ id: 11, name: "Historical attraction" },
+    { id: 1000, name: "Other" }
+  ]);
+
+  // Get location name
+  let locationName = await LocationTypeService.getLocationName(1);
+  expect(locationName).toBe("Hotel");
+  locationName = await LocationTypeService.getLocationName(11);
+  expect(locationName).toBe("Historical attraction");
+  locationName = await LocationTypeService.getLocationName(1000);
+  expect(locationName).toBe("Other");
+  locationName = await LocationTypeService.getLocationName(999);
+  expect(locationName).toBe(undefined);
+
+  // Check valid locations
+  let validLocation = await LocationTypeService.validLocation(2);
+  expect(validLocation).toBe(true);
+  validLocation = await LocationTypeService.validLocation(10);
+  expect(validLocation).toBe(true);
+  validLocation = await LocationTypeService.validLocation(999);
+  expect(validLocation).toBe(false);
 });
