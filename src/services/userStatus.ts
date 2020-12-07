32e2b7632c0cb0313a5d656fdc93579314dbd112
WKHAllen <wkhallen@gmail.com>
2020-12-07 14:57:39 -0600

    Document user status services

--- src/services/userStatus.ts
+++ src/services/userStatus.ts
@@ -1,14 +1,27 @@
+/**
+ * Services for the user status table.
+ * @packageDocumentation
+ */
+
 import mainDB from "./util";
 
-// User status architecture
+/**
+ * User status architecture.
+ */
 export interface UserStatus {
   id: number;
   name: string;
 }
 
-// User status services
+/**
+ * User status services.
+ */
 export module UserStatusService {
-  // Get all user statuses
+  /**
+   * Get all user statuses.
+   * 
+   * @returns A list of all user statuses.
+   */
   export async function getStatuses(): Promise<UserStatus[]> {
     const sql = `SELECT id, name FROM UserStatus ORDER BY id;`;
     const rows: UserStatus[] = await mainDB.execute(sql);
@@ -16,7 +29,12 @@ export module UserStatusService {
     return rows;
   }
 
-  // Get the name of a user status by ID
+  /**
+   * Get the name of a user status by ID.
+   * 
+   * @param statusID A status's ID.
+   * @returns The status's name.
+   */
   export async function getStatusName(statusID: number): Promise<string> {
     const sql = `SELECT name FROM UserStatus WHERE id = ?;`;
     const params = [statusID];
@@ -25,7 +43,12 @@ export module UserStatusService {
     return rows[0]?.name;
   }
 
-  // Check if a user status is valid
+  /**
+   * Check if a user status is valid.
+   * 
+   * @param statusID A status's ID.
+   * @returns Whether or not the status is valid.
+   */
   export async function validStatus(statusID: number): Promise<boolean> {
     const sql = `SELECT id FROM UserStatus WHERE id = ?;`;
     const params = [statusID];
