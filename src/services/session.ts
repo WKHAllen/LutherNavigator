b85a74aecdec1db9bdfb42d05b490576113d3c0a
WKHAllen <wkhallen@gmail.com>
2020-12-07 14:44:21 -0600

    Document session services

--- src/services/session.ts
+++ src/services/session.ts
@@ -1,15 +1,29 @@
+/**
+ * Services for the session table.
+ * @packageDocumentation
+ */
+
 import mainDB, { getTime, newUniqueID, sessionIDLength } from "./util";
 
-// Session architecture
+/**
+ * Session architecture.
+ */
 export interface Session {
   id: string;
   userID: string;
   createTime: number;
 }
 
-// Session services
+/**
+ * Session services.
+ */
 export module SessionService {
-  // Create a session
+  /**
+   * Create a session.
+   * 
+   * @param userID The ID of the user associated with the session.
+   * @returns The new session's ID.
+   */
   export async function createSession(userID: string): Promise<string> {
     const newSessionID = await newUniqueID("Session", sessionIDLength);
 
@@ -26,7 +40,12 @@ export module SessionService {
     return newSessionID;
   }
 
-  // Check if a session exists
+  /**
+   * Check if a session exists.
+   * 
+   * @param sessionID A session's ID.
+   * @returns Whether or not the session exists.
+   */
   export async function sessionExists(sessionID: string): Promise<boolean> {
     const sql = `SELECT id FROM Session WHERE id = ?;`;
     const params = [sessionID];
@@ -35,7 +54,12 @@ export module SessionService {
     return rows.length > 0;
   }
 
-  // Get a session
+  /**
+   * Get a session.
+   * 
+   * @param sessionID A session's ID.
+   * @returns The session.
+   */
   export async function getSession(sessionID: string): Promise<Session> {
     const sql = `SELECT * FROM Session WHERE id = ?;`;
     const params = [sessionID];
@@ -44,14 +68,23 @@ export module SessionService {
     return rows[0];
   }
 
-  // Delete a session
+  /**
+   * Delete a session.
+   * 
+   * @param sessionID A session's ID.
+   */
   export async function deleteSession(sessionID: string): Promise<void> {
     const sql = `DELETE FROM Session WHERE id = ?;`;
     const params = [sessionID];
     await mainDB.execute(sql, params);
   }
 
-  // Get all of a user's sessions
+  /**
+   * Get all of a user's sessions.
+   * 
+   * @param userID A user's ID.
+   * @returns A list of all sessions associated with the user.
+   */
   export async function getUserSessions(userID: string): Promise<Session[]> {
     const sql = `SELECT * FROM Session WHERE userID = ? ORDER BY createTime;`;
     const params = [userID];
@@ -60,14 +93,23 @@ export module SessionService {
     return rows;
   }
 
-  // Delete all of a user's sessions
+  /**
+   * Delete all of a user's sessions.
+   * 
+   * @param userID A user's ID.
+   */
   export async function deleteUserSessions(userID: string): Promise<void> {
     const sql = `DELETE FROM Session WHERE userID = ?;`;
     const params = [userID];
     await mainDB.execute(sql, params);
   }
 
-  // Get a user ID by session ID
+  /**
+   * Get a user ID by session ID.
+   * 
+   * @param sessionID A session's ID.
+   * @returns The ID of the user associated with the session.
+   */
   export async function getUserBySessionID(
     sessionID: string
   ): Promise<string> {
