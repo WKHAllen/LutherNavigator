24a6e9d306e5f97fd6f2a43f9ef924d5ba46ab94
WKHAllen <wkhallen@gmail.com>
2020-12-08 18:40:26 -0600

    Export meta services

--- src/services.ts
+++ src/services.ts
@@ -4,6 +4,7 @@
  */
 export * from "./services/image";
 export * from "./services/locationType";
+export * from "./services/meta";
 export * from "./services/post";
 export * from "./services/rating";
 export * from "./services/session";
