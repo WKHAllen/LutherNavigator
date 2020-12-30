50a42be72953df09cbb85607aefdc46021ba065a
WKHAllen <wkhallen@gmail.com>
2020-12-29 21:31:09 -0600

    Improve formatting of date timestamps

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -6,6 +6,13 @@ function improveTimestamps() {
       parseInt(timestamp.innerText) * 1000
     ).toLocaleString();
   }
+
+  const timestampDates = document.getElementsByClassName("timestamp-date");
+  for (var timestampDate of timestampDates) {
+    timestampDate.innerText = new Date(
+      parseInt(timestampDate.innerText) * 1000
+    ).toDateString();
+  }
 }
 
 // When page loads
