54a4b1b3ecbbadb6c0659a2e80898c6b0e11aec8
WKHAllen <wkhallen@gmail.com>
2020-12-31 18:50:45 -0600

    Use wrapRoute in all async controllers

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -5,6 +5,7 @@
 
 import { Router } from "express";
 import { renderPage, setSessionID } from "./util";
+import wrapRoute from "../asyncCatch";
 import { UserService, SessionService } from "../services";
 
 /**
@@ -13,33 +14,39 @@ import { UserService, SessionService } from "../services";
 export const loginRouter = Router();
 
 // Login page
-loginRouter.get("/", async (req, res) => {
-  await renderPage(req, res, "login", { title: "Login" });
-});
+loginRouter.get(
+  "/",
+  wrapRoute(async (req, res) => {
+    await renderPage(req, res, "login", { title: "Login" });
+  })
+);
 
 // Login event
-loginRouter.post("/", async (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
+loginRouter.post(
+  "/",
+  wrapRoute(async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
 
-  const validLogin = await UserService.login(email, password);
+    const validLogin = await UserService.login(email, password);
 
-  if (validLogin) {
-    const user = await UserService.getUserByEmail(email);
-    const sessionID = await SessionService.createSession(user.id);
-    setSessionID(res, sessionID);
+    if (validLogin) {
+      const user = await UserService.getUserByEmail(email);
+      const sessionID = await SessionService.createSession(user.id);
+      setSessionID(res, sessionID);
 
-    const after = req.query.after as string;
+      const after = req.query.after as string;
 
-    if (after) {
-      res.redirect(after);
+      if (after) {
+        res.redirect(after);
+      } else {
+        res.redirect("/");
+      }
     } else {
-      res.redirect("/");
+      await renderPage(req, res, "login", {
+        title: "Login",
+        error: "Invalid login",
+      });
     }
-  } else {
-    await renderPage(req, res, "login", {
-      title: "Login",
-      error: "Invalid login",
-    });
-  }
-});
+  })
+);
--- src/routes/post.ts
+++ src/routes/post.ts
@@ -5,6 +5,7 @@
 
 import { Router } from "express";
 import { renderPage } from "./util";
+import wrapRoute from "../asyncCatch";
 import { PostService, UserStatusService } from "../services";
 
 /**
@@ -13,19 +14,24 @@ import { PostService, UserStatusService } from "../services";
 export const postRouter = Router();
 
 // Post page
-postRouter.get("/:postID", async (req, res) => {
-  const post = await PostService.getPost(req.params.postID);
-  const user = await PostService.getPostUser(req.params.postID);
-  const userStatusName = await UserStatusService.getStatusName(user.statusID);
+postRouter.get(
+  "/:postID",
+  wrapRoute(async (req, res) => {
+    const post = await PostService.getPost(req.params.postID);
+    const user = await PostService.getPostUser(req.params.postID);
+    const userStatusName = await UserStatusService.getStatusName(
+      user.statusID
+    );
 
-  await renderPage(req, res, "post", {
-    location: post.location,
-    firstname: user.firstname,
-    lastname: user.lastname,
-    status: userStatusName,
-    program: post.program,
-    createTime: post.createTime,
-    threeWords: post.threeWords,
-    content: post.content,
-  });
-});
+    await renderPage(req, res, "post", {
+      location: post.location,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      status: userStatusName,
+      program: post.program,
+      createTime: post.createTime,
+      threeWords: post.threeWords,
+      content: post.content,
+    });
+  })
+);
