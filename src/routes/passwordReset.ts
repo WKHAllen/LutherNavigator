1097d21d842959702de493b2d0361d164b4d623f
WKHAllen <wkhallen@gmail.com>
2021-01-07 15:24:08 -0600

    Add password reset logic

--- src/routes/passwordReset.ts
+++ src/routes/passwordReset.ts
@@ -4,7 +4,12 @@
  */
 
 import { Router } from "express";
-import { renderPage, getHostname } from "./util";
+import {
+  renderPage,
+  getHostname,
+  getErrorMessage,
+  setErrorMessage,
+} from "./util";
 import wrapRoute from "../asyncCatch";
 import { PasswordResetService } from "../services";
 import { sendFormattedEmail } from "../emailer";
@@ -18,7 +23,11 @@ export const passwordResetRouter = Router();
 passwordResetRouter.get(
   "/",
   wrapRoute(async (req, res) => {
-    await renderPage(req, res, "passwordResetRequest");
+    const error = getErrorMessage(req, res);
+
+    await renderPage(req, res, "passwordResetRequest", {
+      error,
+    });
   })
 );
 
@@ -41,7 +50,7 @@ passwordResetRouter.post(
       );
     }
 
-    res.redirect("./request-success");
+    res.redirect("/password-reset/request-success");
   })
 );
 
@@ -59,9 +68,11 @@ passwordResetRouter.get(
   wrapRoute(async (req, res) => {
     const resetID = req.params.resetID;
     const exists = await PasswordResetService.resetRecordExists(resetID);
+    const error = getErrorMessage(req, res);
 
     await renderPage(req, res, "passwordReset", {
       valid: exists,
+      error,
     });
   })
 );
@@ -70,7 +81,32 @@ passwordResetRouter.get(
 passwordResetRouter.post(
   "/reset/:resetID",
   wrapRoute(async (req, res) => {
-    res.redirect("./reset-success");
+    const resetID = req.params.resetID;
+    const password = req.body.password;
+    const password2 = req.body.confirmPassword;
+
+    if (password.length < 8 || password.length > 255) {
+      setErrorMessage(
+        res,
+        "Password must be between 8 and 255 characters in length"
+      );
+      res.redirect(`./${resetID}`);
+    } else if (password !== password2) {
+      setErrorMessage(res, "Passwords do not match");
+      res.redirect(`./${resetID}`);
+    } else {
+      const success = await PasswordResetService.resetPassword(
+        resetID,
+        password
+      );
+
+      if (success) {
+        res.redirect("/password-reset/reset-success");
+      } else {
+        setErrorMessage(res, "Your link has expired. Please try again.");
+        res.redirect("/password-reset");
+      }
+    }
   })
 );
 
