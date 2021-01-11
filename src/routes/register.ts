61ed4dc05a93f62afd538504ce0b9e509670330e
WKHAllen <wkhallen@gmail.com>
2021-01-11 11:30:11 -0600

    Add register form validation

--- src/routes/register.ts
+++ src/routes/register.ts
@@ -4,9 +4,9 @@
  */
 
 import { Router } from "express";
-import { renderPage } from "./util";
+import { renderPage, getErrorMessage, setErrorMessage } from "./util";
 import wrapRoute from "../asyncCatch";
-import { UserStatusService } from "../services";
+import { UserService, UserStatusService } from "../services";
 
 /**
  * The register router.
@@ -17,10 +17,67 @@ export const registerRouter = Router();
 registerRouter.get(
   "/",
   wrapRoute(async (req, res) => {
+    const error = getErrorMessage(req, res);
     const userStatuses = await UserStatusService.getStatuses();
 
     await renderPage(req, res, "register", {
+      error,
       userStatuses,
     });
+  })
+);
+
+// Register event
+registerRouter.post(
+  "/",
+  wrapRoute(async (req, res) => {
+    const firstname: string = req.body.firstname;
+    const lastname: string = req.body.lastname;
+    const email: string = req.body.email;
+    const password: string = req.body.password;
+    const confirmPassword: string = req.body.confirmPassword;
+    const userStatus: number = parseInt(req.body.userStatus) || 0;
+
+    if (firstname.length < 1 || firstname.length > 63) {
+      setErrorMessage(res, "First name must be less than 64 characters");
+      res.redirect("/register");
+      return;
+    }
+
+    if (lastname.length < 1 || lastname.length > 63) {
+      setErrorMessage(res, "Last name must be less than 64 characters");
+      res.redirect("/register");
+      return;
+    }
+
+    if (email.length < 5 || email.length > 63) {
+      setErrorMessage(res, "Email must be less than 64 characters");
+      res.redirect("/register");
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      setErrorMessage(res, "Passwords do not match");
+      res.redirect("/register");
+      return;
+    } else if (password.length < 8) {
+      setErrorMessage(res, "Password must be at least 8 characters");
+      res.redirect("/register");
+      return;
+    }
+
+    const emailUnique = await UserService.uniqueEmail(email);
+    if (!emailUnique) {
+      setErrorMessage(res, "Email address is already in use");
+      res.redirect("/register");
+      return;
+    }
+
+    const validUserStatus = await UserStatusService.validStatus(userStatus);
+    if (!validUserStatus) {
+      setErrorMessage(res, "Invalid user status");
+      res.redirect("/register");
+      return;
+    }
   })
 );
