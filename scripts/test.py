21e132153932c346a8a06ef34f144c55a0e0ee51
WKHAllen <wkhallen@gmail.com>
2021-02-12 16:29:30 -0600

    Move UI tests into test directory

--- scripts/test.py
+++ scripts/test.py
@@ -9,7 +9,7 @@ def main():
     args = " ".join(sys.argv[2:])
     os.system(f"npx jest {args} --coverage --coverageReporters='json-summary' --runInBand")
     os.system("npx jest-coverage-badges")
-    os.system("npx ts-node uitest/uitest.ts")
+    os.system("npx ts-node test/ui/uitest.ts")
 
 
 if __name__ == "__main__":
