93751f61a4f703e5ee4499c249a982f9c50cdb0c
David Oniani <onianidavid@gmail.com>
2021-02-12 09:29:11 -0600

    Code coverage

--- scripts/test.py
+++ scripts/test.py
@@ -8,6 +8,8 @@ def main():
     env.load_env(".env")
     args = " ".join(sys.argv[2:])
     os.system(f"npx jest {args}")
+    os.system("npx jest --coverage --coverageReporters='json-summary'")
+    os.system("npx jest-coverage-badges")
 
 
 if __name__ == "__main__":
