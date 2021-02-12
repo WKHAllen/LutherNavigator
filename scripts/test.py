e8e385820b0d598bbd88d9afae42b3a725c97b5b
David Oniani <onianidavid@gmail.com>
2021-02-12 11:50:52 -0600

    Playwright support

--- scripts/test.py
+++ scripts/test.py
@@ -7,9 +7,9 @@ import env
 def main():
     env.load_env(".env")
     args = " ".join(sys.argv[2:])
-    os.system(f"npx jest {args}")
-    os.system("npx jest --coverage --coverageReporters='json-summary'")
+    os.system(f"npx jest {args} --coverage --coverageReporters='json-summary'")
     os.system("npx jest-coverage-badges")
+    os.system("npx ts-node uitest/uitest.ts")
 
 
 if __name__ == "__main__":
