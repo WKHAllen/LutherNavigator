c6916397fcd5a3c1d9740c0d52227c9789d7b71d
David Oniani <onianidavid@gmail.com>
2021-02-20 20:28:07 -0600

    Device emulation

--- scripts/test.py
+++ scripts/test.py
@@ -4,12 +4,33 @@ import sys
 import env
 
 
-def main():
+def main() -> None:
+    """Process the command line arguments."""
+
+    # Load the environment variables
     env.load_env(".env")
-    args = " ".join(sys.argv[2:])
-    os.system(f"npx jest {args} --coverage --coverageReporters='json-summary' --runInBand")
-    os.system("npx jest-coverage-badges")
-    os.system("npx ts-node test/ui/uitest.ts")
+
+    # Get the arguments
+    args = sys.argv[2:]
+
+    # Optionally specified `emulation-mode` argument MUST BE THE LAST in the
+    # argument list. If the emulation mode is on, in addition to the basic UI
+    # tests using three major web engines, mobile emulation tests on a variety
+    # of devices will also run. Since the emulation can take quite a bit of
+    # time, the `emulation-mode` argument is made optional.
+    if args and args[-1] == "--emulation-mode":
+        os.system(
+            f"npx jest --coverage --coverageReporters='json-summary' --runInBand {' '.join(args[:-1])}"
+        )
+        os.system("npx jest-coverage-badges")
+        os.system("npx ts-node test/ui/uitest.ts")
+        os.system("npx ts-node test/ui/mobile_emulation.ts")
+    else:
+        os.system(
+            f"npx jest --coverage --coverageReporters='json-summary' --runInBand {' '.join(args)}"
+        )
+        os.system("npx jest-coverage-badges")
+        os.system("npx ts-node test/ui/uitest.ts")
 
 
 if __name__ == "__main__":
